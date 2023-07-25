fbae68be0bfdf2dff357dc82443a2c1109127c8a
Krish Kochar <krishkochar@gmail.com>
2023-07-25 23:37:13 +0530

    Updated README & Career Page

--- src/Components/Pages/Career.js
+++ src/Components/Pages/Career.js
@@ -3,7 +3,124 @@ import React from 'react'
 export const Career = () => {
   return (
     <div className='career-page'>
-        Career - This section is coming soon
+        <div class="container">
+              <div class="main-timeline">
+
+                        <div class="timeline">
+                            <div class="icon"></div>
+                            <div class="date-content">
+                                <div class="date-outer">
+                                    <span class="date">
+                                            <span class="month">January</span>
+                                    <span class="year">2020</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="timeline-content">
+                                <h5 class="title">Completed my GCE Cambridge 'O' Levels in Singapore</h5>
+                                <p class="description">
+                                    I grew up in Singapore and stayed there till the age of 16. I completed my 
+                                    'O' Level Examinations at Swiss Cottage Secondary School.
+                                  </p>
+                            </div>
+                        </div>
+
+                        <div class="timeline">
+                            <div class="icon"></div>
+                            <div class="date-content">
+                                <div class="date-outer">
+                                    <span class="date">
+                                            <span class="month">July</span>
+                                    <span class="year">2022</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="timeline-content">
+                                <h5 class="title">Graduated with an IB Diploma in India</h5>
+                                <p class="description">
+                                    After moving to India, I enrolled in the IB Diploma Programme in Shiv Nadar
+                                    School, Gurgaon under a merit scholarship. In July 2022, I graduated high school 
+                                    with an IB Diploma. 
+                                  </p>
+                            </div>
+                        </div>
+
+
+
+                        <div class="timeline">
+                            <div class="icon"></div>
+                            <div class="date-content">
+                                <div class="date-outer">
+                                    <span class="date">
+                                            <span class="month">Fall</span>
+                                    <span class="year">2022</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="timeline-content">
+                                <h5 class="title">Started at the University of Waterloo</h5>
+                                <p class="description">
+                                      In September 2022, I moved to Canada to study Computer Science at the 
+                                      University of Waterloo with the Co-op programme. I was awarded the President's
+                                      Scholarship of Distinction. 
+                                  </p>
+                            </div>
+                        </div>
+
+
+                        <div class="timeline">
+                            <div class="icon"></div>
+                            <div class="date-content">
+                                <div class="date-outer">
+                                    <span class="date">
+                                            <span class="month">Spring</span>
+                                    <span class="year">2023</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="timeline-content">
+                                <h5 class="title">Intern, Engineering at Publicis Sapient</h5>
+                                <p class="description">
+                                    For my first Co-op work term, I interned at Publicis Sapient, particulary working
+                                    in the area of backend development. Here are some highlights; <br/>
+                                    <ul>
+                                      <li>
+                                        Used Go and JS to develop and test backend APIs for a client, most significantly an identity manager functionality for a login system. Identified and resolved several bugs to improve functionality.
+                                      </li>
+                                      <li>
+                                        Collaborated to design backend microservice-based architecture and created JIRA stories for the project
+                                      </li>
+                                      <li>
+                                        Implemented Prompt Engineering techniques in conjunction with ChatGPT to optimize software development processes
+                                      </li>
+                                    </ul>
+                                    </p>
+                            </div>
+                        </div>
+
+
+
+                        <div class="timeline">
+                            <div class="icon"></div>
+                            <div class="date-content">
+                                <div class="date-outer">
+                                    <span class="date">
+                                            <span class="month">Spring</span>
+                                    <span class="year">2024</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="timeline-content">
+                                <h5 class="title">Seeking an Internship for my next Co-op Term!</h5>
+                                <p class="description">
+                                    I'm looking for an internship for the 2024 Spring term! I'm interested
+                                    in many fields, including Full-Stack Dev, Software Dev, AI/ML, and more.
+                                    Feel free to reach out to me anytime!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+            </div>    
     </div>
   )
 }
