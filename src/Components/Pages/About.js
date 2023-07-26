e12b6c584550f0b907655eb03b677eea29d47fd6
Krish Kochar <krishkochar@gmail.com>
2023-07-27 01:00:57 +0530

    Edited About Page

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -3,7 +3,23 @@ import React from 'react'
 export const About = () => {
   return (
     <div className='about-page'>
-        <p>My name is Krish Kochar :)</p>
+      <div className='about-me'>
+        <p> 
+          Hello! <br/> <br/>
+          My name is Krish Kochar, and I am a student starting his second year at the University of Waterloo.
+          I'm majoring in Computer Science and am enrolled in the Co-op programme. 
+          <br/> <br/>
+          My interests lie in the fields of Software Dev, Full-Stack Dev, AI & ML, and more. Over the past year, 
+          I have been exploring these areas, and very much look forward to exploring many other such areas in 
+          the future.
+
+          <br/><br/><br/>
+          You can find me on Social Media!
+
+          <br/><br/><br/><br/><br/><br/>
+          Thanks for visiting my website! Have a nice day!
+        </p>
+      </div>
     </div>
   )
 }
