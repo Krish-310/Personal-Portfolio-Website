592fac6f1d2b9928fc9f5355f140e44ad058e5d1
Krish Kochar <krishkochar@gmail.com>
2023-07-27 01:01:21 +0530

    Added Footer for the website

---
+++ src/Components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import cat from '../Assets/Images/footer_cat.png'
+import logo from '../Assets/Images/Krish_black_cat.png'
+import navIcon1 from '../Assets/Images/nav-icon1.svg'
+import discord from '../Assets/Images/discord-mark-white.svg'
+import instagram from '../Assets/Images/insta.svg'
+import github from '../Assets/Images/github-mark-white.svg'
+
+export const Footer = () => {
+  return (
+        <footer className='footer'>
+            <script src="https://kit.fontawesome.com/597e5024dc.js" crossOrigin="anonymous"></script>
+            <Container>
+                <Row className="align-items-center">
+                    <Col sm={3}>
+                        <div className='cat'>
+                            <img src={cat} alt=''/>
+                        </div>
+                    </Col>
+                    <Col sm={6}>
+                        <div className='logo'>
+                            <a href='#home'>  <img src={logo} alt=''/> </a>
+                        </div>
+                        <div className='wrap'>
+                        <div className='social-icon icon-footer'>
+                            <a href='#>'> <img src={navIcon1} alt=''/> </a>
+                            <a href='#>'> <img src={discord} alt=''/> </a>
+                            <a href='#>'> <img src={instagram} alt=''/> </a>
+                            <a href='#>'> <img src={github} alt=''/> </a>
+                        </div>
+                        </div>
+                    </Col>
+                    <Col sm={3}>
+                        <div className='footer-text'>
+                        <p className='text'> Thanks for visiting my website! <br/>
+                            Last updated July 2023 <br/>
+                            Images sourced from <a href='https://www.vecteezy.com/'> vecteezy.com </a> <br/>
+                            &#169; Copyright 2023. All Rights Reserved. </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+  )
+}
